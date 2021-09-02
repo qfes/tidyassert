b764e5b1c48fdd03e6919d71f8340541f1b5402c
Anthony North <anthony.jl.north@gmail.com>
2021-09-02 17:48:37 +1000

    add logical assertions

---
+++ R/logical-assertions.R
@@ -0,0 +1,117 @@
+#' Assert equal
+#'
+#' Raises an assertion error when `!all(a == b)`.
+#' @name assert_equal
+#' @param a <`any`> any value
+#' @param b <`any`> any value
+#' @param error_message <`string`> the error message.
+#' Accepts placeholders `{a}` and `{b}`, which will be replaced with the unevaluated expressions
+#' for `a` and `b`.
+#'
+#' @family assertions
+#' @export
+assert_equal <- function(a, b,
+                         error_message = "{a} must equal {b}",
+                         error_class = NULL) {
+  assert_(
+    a == b,
+    get_qexpr(!!substitute(a == b)),
+    fmt_message(error_message, a = substitute(a), b = substitute(b)),
+    error_class
+  )
+}
+
+#' Assert not equal
+#'
+#' Raises an assertion error when `!all(a != b)`
+#' @name assert_not_equal
+#'
+#' @inherit assert_equal
+#' @family assertions
+#' @export
+assert_not_equal <- function(a, b,
+                             error_message = "{a} must not equal {b}",
+                             error_class = NULL) {
+  assert_(
+    a != b,
+    get_qexpr(!!substitute(a != b)),
+    fmt_message(error_message, a = substitute(a), b = substitute(b)),
+    error_class
+  )
+}
+
+#' Assert less
+#'
+#' Raises an assertion error when `!all(a < b)`
+#' @name assert_less
+#'
+#' @inherit assert_equal
+#' @family assertions
+#' @export
+assert_less <- function(a, b,
+                        error_message = "{a} must be less than {b}",
+                        error_class = NULL) {
+  assert_(
+    a < b,
+    get_qexpr(!!substitute(a < b)),
+    fmt_message(error_message, a = substitute(a), b = substitute(b)),
+    error_class
+  )
+}
+
+#' Assert less equal
+#'
+#' Raises an assertion error when `!all(a <= b)`
+#' @name assert_less_equal
+#'
+#' @inherit assert_equal
+#' @family assertions
+#' @export
+assert_less_equal <- function(a, b,
+                              error_message = "{a} must be less than or equal {b}",
+                              error_class = NULL) {
+  assert_(
+    a <= b,
+    get_qexpr(!!substitute(a <= b)),
+    fmt_message(error_message, a = substitute(a), b = substitute(b)),
+    error_class
+  )
+}
+
+#' Assert greater
+#'
+#' Raises an assertion error when `!all(a > b)`
+#' @name assert_greater
+#'
+#' @inherit assert_equal
+#' @family assertions
+#' @export
+assert_greater <- function(a, b,
+                           error_message = "{a} must be greater than {b}",
+                           error_class = NULL) {
+  assert_(
+    a > b,
+    get_qexpr(!!substitute(a > b)),
+    fmt_message(error_message, a = substitute(a), b = substitute(b)),
+    error_class
+  )
+}
+
+#' Assert greater equal
+#'
+#' Raises an assertion error when `!all(a >= b)`
+#' @name assert_greater_equal
+#'
+#' @inherit assert_equal
+#' @family assertions
+#' @export
+assert_greater_equal <- function(a, b,
+                                 error_message = "{a} must be greater than or equal {b}",
+                                 error_class = NULL) {
+  assert_(
+    a >= b,
+    get_qexpr(!!substitute(a >= b)),
+    fmt_message(error_message, a = substitute(a), b = substitute(b)),
+    error_class
+  )
+}
--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,30 @@ get_qexpr <- function(expr, env = parent.frame(2L)) {
   rlang::enquo(expr) |>
     rlang::quo_set_env(env)
 }
+
+# format a value
+fmt_value <- function(value) {
+  if (inherits(value, "AsIs")) {
+    return(rlang::as_string(value))
+  }
+
+  if (rlang::is_quosure(value) && rlang::quo_is_symbol(value) || rlang::is_symbol(value)) {
+    return(paste0("`", rlang::as_label(value), "`"))
+  }
+
+  rlang::as_label(value)
+}
+
+# replace placeholders with values from named dots
+fmt_message <- function(templates, ...) {
+  replace <- function(template, x) {
+    placeholder <- paste0("{", x, "}")
+    gsub(placeholder, fmt_value(dots[[x]]), template, fixed = TRUE)
+  }
+
+  fmt_template <- function(template) Reduce(replace, names(dots), template)
+
+  dots <- rlang::dots_list(..., .named = TRUE)
+  lapply(templates, fmt_template) |>
+    unlist()
+}
