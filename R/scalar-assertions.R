ddadcf0b115810e71c8c72abdc6267ac6a7a779e
Anthony North <anthony.jl.north@gmail.com>
2021-09-03 11:07:44 +1000

    add assert_is_string

---
+++ R/scalar-assertions.R
@@ -0,0 +1,20 @@
+#' Assert is string
+#'
+#' Raises an assertion error when `!rlang::is_string(obj)`.
+#' @name assert_is_string
+#' @param obj <`any`> any value
+#' @param error_message <`string`> the error message.
+#' Accepts placeholder `{obj}` which will be replaced with the unevaluated expression for `obj`.
+#'
+#' @family assertions
+#' @export
+assert_is_string <- function(obj,
+                             error_message = c(x = "{obj} must be a <string>"),
+                             error_class = NULL) {
+  assert_(
+    rlang::is_string(obj),
+    quo_expr(rlang::is_string(obj)),
+    fmt_message(error_message, obj = quo_expr(substitute(obj))),
+    error_class
+  )
+}
